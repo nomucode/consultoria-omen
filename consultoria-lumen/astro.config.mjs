7c665ae36161601dc10c9889c31a13423f8f5108
nomucode <noelmrodri@gmail.com>
2025-12-18 17:28:19 +0100

    New build

--- consultoria-lumen/astro.config.mjs
+++ consultoria-lumen/astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luisaplanas.github.io',
-  base: '/',
+  base: '/consultoria-omen',
   outDir: '../docs',
   vite: {
     plugins: [tailwindcss()]
